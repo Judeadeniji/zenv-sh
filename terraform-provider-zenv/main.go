9864d7c9fb88f6c088cb858219e2588d3247a002
Adeniji Oluwaferanmi <judeadeniji66@gmail.com>
2026-05-27 06:23:46 +0100

    Update terraform-provider-zenv/main.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- terraform-provider-zenv/main.go
+++ terraform-provider-zenv/main.go
@@ -21,6 +21,31 @@ func main() {
 
 	err := providerserver.Serve(context.Background(), provider.New(), opts)
 	if err != nil {
-		log.Fatal(err.Error())
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+
+	"github.com/Judeadeniji/zenv-sh/terraform-provider-zenv/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+func main() {
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/judeadeniji/zenv",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(), opts)
+	if err != nil {
+		slog.Error("provider server failed", "error", err)
+		os.Exit(1)
+	}
+}
 	}
 }
